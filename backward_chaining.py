b87db899886922f9e44d47a4013ff962adb7a664
Sungyong Cho <here@sungyongcho.com>
2023-08-20 05:04:39 +0200

    feat: simple reasoning mode implemented

--- backward_chaining.py
+++ backward_chaining.py
@@ -64,32 +64,42 @@ from parse import KnowledgeBaseDAG
 
 def process_elements(kb: KnowledgeBaseDAG, elements, visited):
     stack = []
+    stack_elements = []
     for index, element in enumerate(elements):
         if element == '!':
-            # remove currently stored value in stack first,
-            stack.pop()
-            # and then run eval_expr again
-            next_element = elements[index - 1]
-            negation_result = not eval_expr(
-                kb, next_element, visited)
+            operand = stack.pop()
+            element_operand = stack_elements.pop()
+            if kb.reasoning:
+                if kb.interactive:
+                    print("(expert-system) ", end='')
+                print(f"Applying negation (!) to '{element_operand}'")
+            negation_result = not eval_expr(kb, operand, visited)
             stack.append(negation_result)
-        elif element == '+':
+            stack_elements.append(element_operand)
+        elif element in ('+', '|', '^'):
             right = stack.pop()
             left = stack.pop()
-            result = left and right
+            right_chars = stack_elements.pop()
+            left_chars = stack_elements.pop()
+            if element == '+':
+                result = left and right
+                operation = 'AND'
+            elif element == '|':
+                result = left or right
+                operation = 'OR'
+            elif element == '^':
+                result = left ^ right
+                operation = 'XOR'
+            if kb.reasoning:
+                if kb.interactive:
+                    print("(expert-system) ", end='')
+                print(
+                    f"Applying {operation} ({element}) to '{left_chars} ({left})' and '{right_chars} ({right})'")
             stack.append(result)
-        elif element == '|':
-            right = stack.pop()
-            left = stack.pop()
-            result = left or right
-            stack.append(result)
-        elif element == '^':  # Handling XOR operation
-            right = stack.pop()
-            left = stack.pop()
-            result = left ^ right
-            stack.append(result)
+            stack_elements.append((left_chars, right_chars, element))
         else:
             stack.append(eval_expr(kb, element, visited))
+            stack_elements.append(element)
     return stack.pop()
 
 
@@ -105,12 +115,19 @@ def eval_expr(kb: KnowledgeBaseDAG, query, visited=None):
         visited = set()
 
     if query in kb.facts:
+        if kb.reasoning is True:
+            if kb.interactive:
+                print("(expert-system) ", end='')
+            print(f"Symbol {query} is in facts list, so it is true")
         return True
 
     # if query in rules:
     #     elements = rules[query]
     #     return process_elements(elements, rules, facts)
-
+    if kb.reasoning:
+        if kb.interactive:
+            print("(expert-system) ", end='')
+        print(f"Finding {query} in rules...")
     key_tuple = find_query_in_keys(kb.rules, query)
 
     if key_tuple in visited:
@@ -118,6 +135,10 @@ def eval_expr(kb: KnowledgeBaseDAG, query, visited=None):
 
     if key_tuple:
         elements = kb.rules[key_tuple]
+        if kb.reasoning:
+            if kb.interactive:
+                print("(expert-system) ", end='')
+            print(f"For query {query}, rule {elements} is found.")
         for element in elements:
             visited_copy = visited.copy()
             visited_copy.add(key_tuple)
@@ -134,9 +155,23 @@ def eval_expr(kb: KnowledgeBaseDAG, query, visited=None):
 
 
 def eval_query(kb: KnowledgeBaseDAG, queries):
+    if kb.reasoning is True:
+        if kb.interactive:
+            print("(expert-system) ", end='')
+        print(f"Evaluating queries {list(queries)}")
     results = {}
     for query in queries:
-        results[query] = eval_expr(kb, query)
+        if kb.reasoning:
+            if kb.interactive:
+                print("(expert-system) ", end='')
+            print(f"Evaluating query {query}")
+        query_result = eval_expr(kb, query)
+        if kb.reasoning:
+            if kb.interactive:
+                print("(expert-system) ", end='')
+            print(
+                f"Result of query {query} is {'True' if query_result else 'False'} ")
+        results[query] = query_result
     return results
 
 
