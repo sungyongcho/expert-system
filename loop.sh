fabd784e52d261966c18320c428e4c5548142c0c
jmcheon <developer.cheon@gmail.com>
2023-08-30 18:12:47 +0200

    chore: remove files

--- loop.sh
+++ loop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 run() {
 	test_input="$1$2"
 	for f in $test_input; do
@@ -13,7 +12,5 @@ test_dir="./tests/_examples/bad_files/"
 test_list=$(ls $test_dir)
 #echo $test_list
 for test_file in $test_list; do
-	#echo "#Result should be Error" >> $f
 	run $test_dir $test_file
-
 done
